ba511aafe6e4a265f6ae6ddc3fe5e202f006c050
kenwata <watanabe.sry@gmail.com>
2015-12-07 19:40:22 +0900

    edit dotfilesLink

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.gvim ~/.gvimrc
-ls -s ~/dotfiles/.gitconfig ~/.gitconfig
+# gitconfigファイル
+# 必要に応じてコメントを取る
+#ls -s ~/dotfiles/.gitconfig ~/.gitconfig
+# NeoBundle
+# もう入っている場合はいらない
+#mkdir ~/.vim/bundle
+#git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
+
