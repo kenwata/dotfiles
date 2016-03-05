534e63cf87a5b9fd4d12fb8660af9555f20272d4
kenwata <watanabe.sry@gmail.com>
2016-03-05 09:35:23 +0900

    edit shell script

--- .shell/dotfilesLink.sh
+++ .shell/dotfilesLink.sh
@@ -4,9 +4,9 @@
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.gvim ~/.gvimrc
 
-# 分割しているvim設定ファイルをコピー
+# ~/.vim/に、分割しているvim設定ファイルのシンボリックリンクを張る
 mkdir  ~/.vim
-cp -r ~/dotfiles/.vim/userautoload ~/.vim
+ln -s  ~/dotfiles/.vim/userautoload ~/.vim/
 
 # NeoBundle
 # もう入っている場合はいらない
@@ -14,7 +14,7 @@ mkdir ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
 
 # fileTypeごとの設定(indentなど)
-mkdir ~/.vim/after/ftplugin
+mkdir -p ~/.vim/after/ftplugin
 cp ~/dotfiles/.ftplugin/* ~/.vim/after/ftplugin/
 
 
